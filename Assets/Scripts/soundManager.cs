d085e06a355c11ceae456a5626562da3180f234b
Ori Suchy <orisuchy@gmail.com>
2021-06-01 21:35:35 +0300

    Added sound on/off button and some sprites

---
+++ Assets/Scripts/soundManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundManager : MonoBehaviour
+{
+    [SerializeField] Image soundOnIcon;
+    [SerializeField] Image soundOffIcon;
+    private bool muted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+        }
+        Load();
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
+    }
+
+    public void OnButtonPress()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            if (muted)
+            {
+                muted = false;
+                AudioListener.pause = false;
+            }
+            else
+            {
+                muted = true;
+                AudioListener.pause = true;
+            }
+            Save();
+            UpdateButtonIcon();
+        }
+    }
+
+    private void UpdateButtonIcon()
+    {
+        if (muted)
+        {
+            soundOnIcon.enabled = false;
+            soundOffIcon.enabled = true;
+        }
+        else
+        {
+            soundOnIcon.enabled = true;
+            soundOffIcon.enabled = false;
+        }
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
