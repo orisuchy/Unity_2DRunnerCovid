4e569c6b9381365238a40b0ff1319766e6dbf23e
Ori Suchy <orisuchy@gmail.com>
2021-07-05 13:55:33 +0300

    Change mask color

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    //[SerializeField] private Texture2D cursorTexture;
+    public Texture2D cursorTexture;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(0, 0), CursorMode.Auto);
+    }
+
     public void LoadScene(string name){
         SceneManager.LoadScene(name);
     }
