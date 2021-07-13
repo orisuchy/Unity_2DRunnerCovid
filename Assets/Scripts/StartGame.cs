67a415c35a7265337b88d0f64213c75303746eab
Ori Suchy <orisuchy@gmail.com>
2021-07-13 17:14:25 +0300

    added quit button and esc options

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -13,8 +13,26 @@ public class StartGame : MonoBehaviour
         // An idea to change the cursor... maybe later on
         //Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
+    
+    void Update()
+    {
+        bool escPressed = Input.GetKey(KeyCode.Escape);
+        if (escPressed)
+        {
+            if (SceneManager.GetActiveScene().name == "StartMenu2")
+                QuitGame();
+            else
+                LoadScene("StartMenu2");
+        }
+    }
 
     public void LoadScene(string name){
         SceneManager.LoadScene(name);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
